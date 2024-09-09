f1d8be34d463b179c1a2b7522a1511f9cc92d9c4
Alessandro Ghidini <ghidello@users.noreply.github.com>
2024-09-09 17:48:28 +0200

    Initial solution (#1)
    * initial solution and server project
    
    * add verify pull request github workflow
    
    * add minver dependency and configuration
    
    * show logo in readme

---
+++ test/Server.Tests.Unit/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Alessandro Ghidini. All rights reserved.
+// SPDX-License-Identifier: MIT.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sotsera.Blazor.Server.Tests.Unit;
+
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddSecurityHeaders_ShouldNotRaiseExceptions()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        Action act = () => serviceCollection.AddSecurityHeaders();
+
+        act.Should().NotThrow<ArgumentNullException>();
+    }
+}
